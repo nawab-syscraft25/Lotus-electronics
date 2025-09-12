2cecccedd5aafcbd9b8ab70bff6f5f67bc71556d
nawabkh2040-cloud <nawab22116cst@sdbc.ac.in>
2025-09-12 11:25:30 +0530

    Updated with gemini

--- .history/chat_gemini_20250911172052.py
+++ .history/chat_gemini_20250911172052.py
@@ -776,8 +776,8 @@ import os
 google_api_key = os.getenv("GOOGLE_API_KEY")
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    temperature=0,
-    max_retries=3,
+    temperature=0.7,
+    max_retries=2,
     google_api_key=google_api_key,
 )
 
--- .history/chat_gemini_20250912112242.py
+++ .history/chat_gemini_20250912112242.py
@@ -777,7 +777,7 @@ google_api_key = os.getenv("GOOGLE_API_KEY")
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0,
-    max_retries=3,
+    max_retries=2,
     google_api_key=google_api_key,
 )
 
